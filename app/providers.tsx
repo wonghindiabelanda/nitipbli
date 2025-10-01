b103b5d259411a56e8f32479157963be4e678c25
Jawaers#1 <119018036+wonghindiabelanda@users.noreply.github.com>
2025-10-01 10:22:35 +0700

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
-import { ThemeProvider } from "next-themes"
+import { ThemeProvider } from "next-themes";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
       {children}
     </ThemeProvider>
-  )
+  );
 }
