5389592f08fbf4794c8a311e02200ec8f523ef5d
Jawaers#1 <119018036+wonghindiabelanda@users.noreply.github.com>
2025-10-02 00:07:32 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: "class", // wajib kalau pake next-themes
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
