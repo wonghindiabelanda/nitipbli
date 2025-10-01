9b71bf86e930588062a1d2f91576e79581c48acc
Jawaers#1 <119018036+wonghindiabelanda@users.noreply.github.com>
2025-10-01 10:22:11 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  darkMode: "class",
+  darkMode: "class", // wajib kalau pake next-themes
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
